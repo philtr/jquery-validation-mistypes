217b57a2844ac4f38285fbb2e20d2c06417d5f44
Phillip Ridlen <phillip@ridlen.net>
2010-03-16 12:33:01 -0500

    partially working: just flags it as a possible mistype, no suggestion

--- jquery.validate.mistypes.js
+++ jquery.validate.mistypes.js
@@ -9,16 +9,9 @@
     * jquery.validate.js
 */
 
-var message;
-
-jQuery.validator.addMethod("email-mistypes", function(value, element, params) {
-  
-  //same as 'email': first check to see if it's valid
-  if (/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i.test(value)) {
-  
+jQuery.validator.addMethod("mistypes", function(value, element) {
+    
     flag = false;
-  
-  
     // Dictionary of common misspellings
     // Format: "wrong":"right"
     var commonMisspellings = jQuery.parseJSON('{' +
@@ -78,31 +71,13 @@ jQuery.validator.addMethod("email-mistypes", function(value, element, params) {
     
     // need to figure out how to separate domain and tld from value
     // then return suggested result
-    user = "pridlen";
-    domain = "yahooo";
-    tld = "cmo";
+    url_parts = value.split("@")[1].split(".");
     
-    if(commonMisspellings[domain]){
-      domain = commonMisspellings[domain];
-      flag = true;
-    }
+    for(i=0; i < url_parts.length; i++)
+      if(commonMisspellings[url_parts[i]])
+        flag = true;
     
-    if(commonMisspellings[tld]){
-      tld = commonMisspellings[tld];
-      flag = true;
-    }
+    if(flag) return false;
+    else return true;
     
-    if(flag) {
-      message = "Did you mean: " + user + "@" + domain + "." + tld + "?";
-      return false;
-    } else return true;
-          
-  } else {
-    if(this.optional(element)) { 
-      return true; 
-    } else {
-      message = "Please enter a valid e-mail address";
-      return false;
-    }
-  }
-}, jQuery. );
+}, "Did you mistype your e-mail address?" );
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 #jQuery.validate.mistype.js
-## Addon for http://bassistance.de/jquery-plugins/jquery-plugin-validation/ written to include common e-mail mistakes.
+## Addon for http://bassistance.de/jquery-plugins/jquery-plugin-validation/ written to include common typing errors.
 
 _e.g._
 
@@ -8,9 +8,14 @@ _e.g._
   * "user@yahoo.cmo" => "Did you mean: user@yahoo.com?"
   
 
-**Current Status**: Broken, never working.
+**Current Status**: Partially working.
 
 **Comments**: 
+
+Will display "Did you mistype your e-mail address?" if any part (separated by a ".") of the URL matches a common misspelling in the index. This is a problem, because it will either not catch or false positive the following valid e-mail addresses.
+  * bob@*.co.uk (matches because "co" is a common mistype for "com")
+  * bob@*.cm (which also happens to be a common mistype)
+  
 
 The problem is with the way that the jquery validation plugin is written. The jQuery.validator.addMethod() function takes in three parameters:
 
@@ -18,4 +23,6 @@ The problem is with the way that the jquery validation plugin is written. The jQ
   * function for validation (returns boolean)  
   * message (to display to the user if input is invalid)
   
-The issue is that there is no way to get any additional output from the added validation method other than true/false.  We can find matches and format an output string, but there's no way for it to display to the user, because the message parameter of the addMethod() function adds the message at load time. That is, it evaluates any input immediately. I have to find a way to get other information out of the validation function in order to display a suggested e-mail address.
+The issue is that there is no way to get any additional output from the added validation method other than true/false.  We can find matches and format an output string, but there's no way for it to display to the user, because the message parameter of the addMethod() function adds the message at load time. That is, it evaluates any input immediately. I have to find a way to get other information out of the validation function in order to display a suggested e-mail address. Even when I set a global variable (I know, bad form), the value of it gets evaluated before I can set it in the validation function.
+
+Additionally, it seems that we _cannot chain validators._ Only one validation method is applicable to each input.
